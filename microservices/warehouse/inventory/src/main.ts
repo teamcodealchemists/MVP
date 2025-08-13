f74aea7a1928334e3d474c06c6fb71a7f954db14
sebamarana <58631437+sebamarana@users.noreply.github.com>
2025-08-13 16:37:59 +0200

    Added Serializer/Deserializer and made microservice

--- microservices/warehouse/inventory/src/main.ts
+++ microservices/warehouse/inventory/src/main.ts
@@ -1,8 +1,24 @@
 import { NestFactory } from '@nestjs/core';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 import { AppModule } from './app.module';
+import { Logger } from '@nestjs/common';
+
+import { OutboundResponseSerializer } from './interfaces/nats/natsMessagesFormatters/outbound-response.serializer';
+import { InboundRequestDeserializer } from './interfaces/nats/natsMessagesFormatters/inbound-response.deserializer';
+
+const logger = new Logger();
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(AppModule, {
+    logger: logger,
+    transport: Transport.NATS,
+    options: {
+      servers: ['nats://nats:4222'], // Nome del container NATS
+      deserializer: new InboundRequestDeserializer(),
+      serializer: new OutboundResponseSerializer(),
+    },
+  });
+  await app.listen();
 }
+
 bootstrap();
