b61a471f6a59a5aa44e1ae6973aa17ddf749cfb1
sebamarana <58631437+sebamarana@users.noreply.github.com>
2025-08-25 17:05:59 +0200

    Aggiunto README e Prime configurazioni Auth

--- microservices/centralized/authentication/src/main.ts
+++ microservices/centralized/authentication/src/main.ts
@@ -1,8 +1,25 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { AuthModule } from './application/authentication.module'; //Modulo Principale
+import { NestFactory } from '@nestjs/core'; //Factory per creare il servizio nest
+import { MicroserviceOptions, Transport } from '@nestjs/microservices'; //Opzioni
+import { Logger } from '@nestjs/common'; //Per fare logging del microservizio
+
+//Importanti da copiare in seguito, permettono il formatting dei messaggi in entrata ed uscita a nats
+import { OutboundResponseSerializer } from './interfaces/nats/natsMessagesFormatters/outbound-response.serializer';
+import { InboundRequestDeserializer } from './interfaces/nats/natsMessagesFormatters/inbound-response.deserializer';
+
+const logger = new Logger();
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(AuthModule, {
+    logger: logger,
+    transport: Transport.NATS,
+    options: {
+      servers: ['nats://nats:4222'], // Indirizzo del container NATS
+      deserializer: new InboundRequestDeserializer(),
+      serializer: new OutboundResponseSerializer(),
+    },
+  });
+  await app.listen();
 }
+
 bootstrap();
