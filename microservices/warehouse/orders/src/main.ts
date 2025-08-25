ef209ebc4dbbe231da1b7a22faf139f482dcf988
ShizuIT <shiizu233@gmail.com>
2025-08-25 16:55:12 +0200

    Implementati Serializer e Deserializer per NATS + installato "uuid"

--- microservices/warehouse/orders/src/main.ts
+++ microservices/warehouse/orders/src/main.ts
@@ -2,17 +2,17 @@ import { OrdersModule } from './application/orders.module';
 import { NestFactory } from '@nestjs/core';
 import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 
-/* import { OutboundResponseSerializer } from './interfaces/nats/natsMessagesFormatters/outbound-response.serializer';
+import { OutboundResponseSerializer } from './interfaces/nats/natsMessagesFormatters/outbound-response.serializer';
 import { InboundRequestDeserializer } from './interfaces/nats/natsMessagesFormatters/inbound-response.deserializer';
- */
+
 
 async function bootstrap() {
   const app = await NestFactory.createMicroservice<MicroserviceOptions>(OrdersModule, {
     transport: Transport.NATS,
     options: {
       servers: ['nats://nats:4222'], // Nome del container NATS
-/*       deserializer: new InboundRequestDeserializer(),
-      serializer: new OutboundResponseSerializer(),*/
+      deserializer: new InboundRequestDeserializer(),
+      serializer: new OutboundResponseSerializer(),
       queue: 'orders-queue',
       parser: 'json',
     },
