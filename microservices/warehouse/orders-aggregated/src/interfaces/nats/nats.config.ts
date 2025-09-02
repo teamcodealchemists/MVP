e36074b324f0ed1b8175b20dee676825fd841171
ShizuIT <shiizu233@gmail.com>
2025-09-02 11:53:50 +0200

    Creazione anche nel Dockerfile del microservizio orders-aggregated + fix nei vari file per collegarlo dove dovuto

--- microservices/warehouse/orders-aggregated/src/interfaces/nats/nats.config.ts
+++ microservices/warehouse/orders-aggregated/src/interfaces/nats/nats.config.ts
@@ -4,7 +4,7 @@ export const natsConfig = {
   transport: Transport.NATS,
   options: {
     servers: [process.env.NATS_URL || 'nats://localhost:4222'],
-    queue: 'orders-queue',
+    queue: 'orders-aggregated-queue',
     timeout: 5000,
     maxReconnectAttempts: -1,
     reconnect: true,
