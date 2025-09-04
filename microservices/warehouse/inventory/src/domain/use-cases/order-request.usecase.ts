3ca2dd096f2579ef749771a1fd57592cacf1906d
IwantBeRich <alex.shu@studenti.unipd.it>
2025-09-04 16:58:13 +0200

    modifica

--- microservices/warehouse/inventory/src/domain/use-cases/order-request.usecase.ts
+++ microservices/warehouse/inventory/src/domain/use-cases/order-request.usecase.ts
@@ -1,6 +1,6 @@
 import { productQuantityArrayDto } from 'src/interfaces/dto/productQuantityArray.dto';
 
 export interface OrderRequestUseCase {
-  orderRequest(dto: productQuantityArrayDto): Promise<boolean>;
+  orderRequest(dto: productQuantityArrayDto): Promise<void>;
   shipOrderRequest(dto : productQuantityArrayDto) : Promise<void>;
 }
--- microservices/warehouse/inventory/src/domain/use-cases/product-add-quantity.usecase.ts
+++ microservices/warehouse/inventory/src/domain/use-cases/product-add-quantity.usecase.ts
@@ -1,4 +1,3 @@
-import { Injectable } from '@nestjs/common';
 import { ProductQuantityDto } from 'src/interfaces/dto/productQuantity.dto';
 export interface ProductAddQuantityUseCase {
   addQuantity(dto: ProductQuantityDto): Promise<void> ;
