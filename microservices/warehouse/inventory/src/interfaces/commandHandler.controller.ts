02734f40d405035ad611fa6bb8de38f12501f6a6
IwantBeRich <alex.shu@studenti.unipd.it>
2025-09-04 17:32:27 +0200

    modifica

--- microservices/warehouse/inventory/src/interfaces/commandHandler.controller.ts
+++ microservices/warehouse/inventory/src/interfaces/commandHandler.controller.ts
@@ -1,15 +1,10 @@
 import { Controller, Logger } from '@nestjs/common';
 import { MessagePattern } from '@nestjs/microservices';
-import { InventoryService } from 'src/application/inventory.service';
 import { ProductDto } from './dto/product.dto';
 import { ProductIdDto } from './dto/productId.dto';
 import { DataMapper } from '../infrastructure/mappers/dataMapper';
-import { plainToInstance } from 'class-transformer';
 import { Product } from 'src/domain/product.entity';
 import { Inventory } from 'src/domain/inventory.entity';
-import { Payload } from '@nestjs/microservices';
-import { OutboundEventAdapter } from 'src/infrastructure/adapters/outbound-event.adapter';
-import { WarehouseId } from 'src/domain/warehouseId.entity';
 import { InboundEventListener } from 'src/infrastructure/adapters/inbound-event.adapter';
 
 const logger = new Logger('commandHandler');
