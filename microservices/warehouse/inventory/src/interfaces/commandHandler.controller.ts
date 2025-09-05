f78a75d423980b4baa35b9348952af5f25b8b58e
sebamarana <58631437+sebamarana@users.noreply.github.com>
2025-09-05 11:14:39 +0200

    CommandHandler RESGATE ready

--- microservices/warehouse/inventory/src/interfaces/commandHandler.controller.ts
+++ microservices/warehouse/inventory/src/interfaces/commandHandler.controller.ts
@@ -1,5 +1,5 @@
 import { Controller, Logger } from '@nestjs/common';
-import { Ctx, MessagePattern } from '@nestjs/microservices';
+import { Ctx, MessagePattern, Payload } from '@nestjs/microservices';
 import { ProductDto } from './dto/product.dto';
 import { ProductIdDto } from './dto/productId.dto';
 import { DataMapper } from '../infrastructure/mappers/dataMapper';
@@ -7,6 +7,7 @@ import { Product } from 'src/domain/product.entity';
 import { Inventory } from 'src/domain/inventory.entity';
 import { InboundEventListener } from 'src/infrastructure/adapters/inbound-event.adapter';
 import { validateOrReject } from 'class-validator';
+import { log } from 'console';
 
 
 //TODO: Risistemare per RESGATE
@@ -15,100 +16,92 @@ import { validateOrReject } from 'class-validator';
 const logger = new Logger('commandHandler');
 @Controller()
 export class CommandHandler {
-  constructor(private readonly inboundEventListener : InboundEventListener) { }
+  constructor(private readonly inboundEventListener: InboundEventListener) { }
 
   @MessagePattern(`call.warehouse.${process.env.WAREHOUSE_ID}.stock.new`)
-  async handleNewStock(payload: any): Promise<void> {
-    const data = typeof payload === 'string' ? payload : payload?.data ? payload.data.toString() : payload;
+  async handleNewStock(@Payload('params') payload: any): Promise<string> {
 
-    const productObj = JSON.parse(data);
+    const productObj = payload;
 
-    const productDTO: ProductDto = {
-      id: productObj.id,
-      name: productObj.name,
-      unitPrice: productObj.unitPrice,
-      quantity: productObj.quantity,
-      quantityReserved: productObj.quantityReserved ?? 0,
-      minThres: productObj.minThres,
-      maxThres: productObj.maxThres,
-      warehouseId: productObj.warehouseId
-    };
+    logger.log('Received new stock payload:', productObj);
+
+    const productDTO: ProductDto = new ProductDto();
+    productDTO.id = productObj.id;
+    productDTO.name = productObj.name;
+    productDTO.unitPrice = productObj.unitPrice;
+    productDTO.quantity = productObj.quantity;
+    productDTO.quantityReserved = productObj.quantityReserved ?? 0;
+    productDTO.minThres = productObj.minThres;
+    productDTO.maxThres = productObj.maxThres;
+    productDTO.warehouseId = productObj.warehouseId ?? process.env.WAREHOUSE_ID;
+
     try {
       await validateOrReject(productDTO);
-      await this.inboundEventListener.newStock(productDTO);
-      return Promise.resolve();
+      //await this.inboundEventListener.newStock(productDTO);
+      let RID = `warehouse.${process.env.WAREHOUSE_ID}.stock.${productDTO.id}`;
+      return Promise.resolve(JSON.stringify({ resource: { rid: RID } }));
     } catch (error) {
       logger.error('Error in handleNewStock:', error);
-      return Promise.resolve();
-      //return Promise.resolve(JSON.stringify({ error: { code: 'system.internalError', message: error?.message || 'Unknown error' } }));
+      return await this.errorHandler(error);
     }
   }
 
 
   @MessagePattern(`call.warehouse.${process.env.WAREHOUSE_ID}.stock.*.delete`)
-  async handleRemoveStock(@Ctx() context:any): Promise<void> {
+  async handleRemoveStock(@Ctx() context: any): Promise<string> {
 
     // Estrae l'ID prodotto dalla subject del messaggio, dove l'asterisco (*) rappresenta l'ID
     const subjectParts = context.getSubject().split('.');
     const itemIdStr = subjectParts[subjectParts.length - 2] ?? null;
 
 
-    const productIdDTO: ProductIdDto = {
-      id: itemIdStr
-    };
+    const productIdDTO: ProductIdDto = new ProductIdDto();
+    productIdDTO.id = itemIdStr;
 
     try {
       await validateOrReject(productIdDTO);
-      await this.inboundEventListener.removeStock(productIdDTO);
-      return Promise.resolve();
+      //await this.inboundEventListener.removeStock(productIdDTO);
+      return Promise.resolve(JSON.stringify({ result: `Prodotto con ID ${itemIdStr} rimosso` }));
     } catch (error) {
       logger.error('Error in handleRemoveStock:', error);
-      return Promise.resolve();
-      //return Promise.resolve(JSON.stringify({ error: { code: 'system.internalError', message: error?.message || 'Unknown error' } }));
+      return await this.errorHandler(error);
     }
   }
 
+  @MessagePattern(`call.warehouse.${process.env.WAREHOUSE_ID}.stock.*.set`)
+  async handleEditStock(@Payload('params') payload: any, @Ctx() context: any): Promise<string> {
 
+    const subjectParts = context.getSubject().split('.');
+    const itemIdStr = subjectParts[subjectParts.length - 2] ?? null;
 
-  @MessagePattern(`call.warehouse.${process.env.WAREHOUSE_ID}.stock.set`) //call.warehouse.variabile.stock.#stock.set
-  //fare @payload
-  async handleEditStock(payload: any): Promise<void> {
-     
-    const data =
-      typeof payload === 'string'
-        ? payload
-        : payload?.data
-          ? payload.data.toString()
-          : payload;
+    const productObj = payload;
 
-    const productObj = JSON.parse(data);
+    logger.log('Received edit stock payload:', productObj);
 
+    const productDTO: ProductDto = new ProductDto();
+    productDTO.id = itemIdStr;
+    productDTO.name = productObj.name;
+    productDTO.unitPrice = productObj.unitPrice;
+    productDTO.quantity = productObj.quantity;
+    productDTO.quantityReserved = productObj.quantityReserved ?? 0;
+    productDTO.minThres = productObj.minThres;
+    productDTO.maxThres = productObj.maxThres;
+    productDTO.warehouseId = productObj.warehouseId ?? process.env.WAREHOUSE_ID;
 
-    const productDTO: ProductDto = {
-      id: productObj.id,
-      name: productObj.name,
-      unitPrice: productObj.unitPrice,
-      quantity: productObj.quantity,
-      quantityReserved: productObj.quantityReserved ?? 0,
-      minThres: productObj.minThres,
-      maxThres: productObj.maxThres,
-      warehouseId : productObj.warehouseId
-    };
     try {
       await validateOrReject(productDTO);
-      await this.inboundEventListener.editStock(productDTO);
-      return Promise.resolve();
+      //await this.inboundEventListener.editStock(productDTO);
+      return Promise.resolve(JSON.stringify({ result: `Prodotto con ID ${itemIdStr} aggiornato` }));
     } catch (error) {
       logger.error('Error in handleEditStock:', error);
-      return Promise.resolve();
-      //return Promise.resolve(JSON.stringify({ error: { code: 'system.internalError', message: error?.message || 'Unknown error' } }));
+      return await this.errorHandler(error);
     }
   }
 
 
 
   @MessagePattern(`get.warehouse.${process.env.WAREHOUSE_ID}.stock.*`)
-  async handleGetProduct(@Ctx() context:any): Promise<string> {
+  async handleGetProduct(@Ctx() context: any): Promise<string> {
 
     // Estrae l'ID prodotto dalla subject del messaggio, dove l'asterisco (*) rappresenta l'ID
     const itemIdStr = context.getSubject().split('.').pop();
@@ -119,20 +112,32 @@ export class CommandHandler {
     try {
       await validateOrReject(productIdDTO);
       //return Promise.resolve(await this.inboundEventListener.handleGetProduct(productIdDTO));
-      return Promise.resolve(JSON.stringify({ result: { model: productIdDTO}}));
+      return Promise.resolve(JSON.stringify({ result: { model: productIdDTO } }));
     } catch (error) {
       logger.error('Error in handleGetProduct:', error);
-      //return Promise.resolve(null);
-      return Promise.resolve(JSON.stringify({ error: { code: 'system.internalError', message: Array.isArray(error) ? error.map(e => e.toString()).join(', ') : error?.message || 'Unknown error' } }));
+      return await this.errorHandler(error);
     }
   }
 
-
-
   @MessagePattern(`get.warehouse.${process.env.WAREHOUSE_ID}.inventory`)
-  async handleGetInventory(): Promise<Inventory> {
-    return Promise.resolve(await this.inboundEventListener.getInventory());
+  async handleGetInventory(): Promise<string> {
+    return Promise.resolve(JSON.stringify({ result: await this.inboundEventListener.getInventory() }));
   }
 
+
+  private async errorHandler(error: any): Promise<string> {
+    let message: string;
+      if (Array.isArray(error)) {
+        // class-validator errors: estrai i messaggi di constraint
+        message = error
+          .map(e => Object.values(e.constraints ?? {}).join(', '))
+          .filter(Boolean)
+          .join('; ');
+
+        return Promise.resolve(JSON.stringify({ error: { code: 'system.invalidParams', message } }));
+      } else {
+        return Promise.resolve(JSON.stringify({ error: { code: 'system.internalError', message: error?.message || 'Unknown error' } }));
+      }
+  }
 }
 
